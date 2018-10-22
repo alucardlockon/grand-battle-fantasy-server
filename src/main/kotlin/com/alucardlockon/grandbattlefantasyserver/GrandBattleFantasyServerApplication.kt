980b03b4d0515f30c90b906acc02edb1a6be8e8c
yuwh <alucardlockon3@gmail.com>
2018-10-22 19:54:15 +0800

    reinit

---
+++ src/main/kotlin/com/alucardlockon/grandbattlefantasyserver/GrandBattleFantasyServerApplication.kt
@@ -0,0 +1,11 @@
+package com.alucardlockon.grandbattlefantasyserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrandBattleFantasyServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrandBattleFantasyServerApplication>(*args)
+}
