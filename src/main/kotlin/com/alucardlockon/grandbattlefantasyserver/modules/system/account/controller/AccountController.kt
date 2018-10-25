3e6637cca5206a6a60471b7d17068022f58932d0
yuwh <alucardlockon3@gmail.com>
2018-10-25 16:41:09 +0800

    update gradle

--- src/main/kotlin/com/alucardlockon/grandbattlefantasyserver/modules/system/account/controller/AccountController.kt
+++ src/main/kotlin/com/alucardlockon/grandbattlefantasyserver/modules/system/account/controller/AccountController.kt
@@ -3,9 +3,11 @@ package com.alucardlockon.grandbattlefantasyserver.modules.system.account.contro
 import com.alucardlockon.grandbattlefantasyserver.base.api.ApiResult
 import com.alucardlockon.grandbattlefantasyserver.modules.system.account.entity.Account
 import com.alucardlockon.grandbattlefantasyserver.modules.system.account.service.impl.AccountService
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import java.sql.Wrapper
 
 
 @RestController
@@ -25,12 +27,14 @@ class AccountController {
     }
 
     @PostMapping("/")
-    fun post(account: Account): ApiResult {
+    fun post(@RequestBody account: Account): ApiResult {
+        // TODO: 加密
+        account.password = account.password
         return ApiResult(service.save(account))
     }
 
     @PutMapping("/{id}")
-    fun put(@PathVariable id: Int, account: Account): ApiResult {
+    fun put(@PathVariable id: Int,@RequestBody account: Account): ApiResult {
         account.id = id
         return ApiResult(service.updateById(account))
     }
@@ -38,5 +42,10 @@ class AccountController {
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Int): ApiResult {
         return ApiResult(service.removeById(id))
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody account: Account): ApiResult {
+        return ApiResult(service.getOne(QueryWrapper<Account>(Account(username = account.username,password = account.password))))
     }
 }
